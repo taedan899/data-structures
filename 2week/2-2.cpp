6302cb355ec59830e393b09f2e1d1ea5ec4cb1fd
태단.C <168528128+taedan899@users.noreply.github.com>
2026-03-17 13:10:04 +0900

    Add files via upload

---
+++ 2week/2-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+void main()
+{
+	int i;
+
+	int score[3] = { 91,86,97 };
+	char grade[3] = { 'A','B','C'};
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("%3d학년 : 총점 %d ,등급 : %c\n", i, score[i], grade[i]);
+
+
+	}
+
+
+	getchar();
+
+
+}
---
+++ 2week/2-4.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+void main()
+{
+	char str[20] = "Data structure";	
+	int i;
+	
+	printf("\n문자배열 str[]:");
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		printf("%c", str[i]);
+
+
+	}
+	
+
+}
