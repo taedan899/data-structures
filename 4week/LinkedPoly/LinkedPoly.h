d73570a4fa2b3be4adae8150434ba6ae44f508e1
태단.C <168528128+taedan899@users.noreply.github.com>
2026-05-11 03:15:27 +0900

    Add LinkedPoly header file with linked list structures

---
+++ 4week/LinkedPoly/LinkedPoly.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdlib.h>
+
+typedef struct ListNode {
+	float coef;
+	int expo;
+	struct ListNode* link;
+}ListNode;
+
+typedef struct ListHead{
+
+	ListNode* head;
+}ListHead;
+
+ListHead* createLinkedList(void);
+void appendTerm(ListHead* L, float coef, int expo);
+void addPoly(ListHead* A, ListHead* B, ListHead* C);
+void printPoly(ListHead* L);
